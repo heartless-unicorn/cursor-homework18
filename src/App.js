03972a3be5a383d8abe835170a4357fea8e81c63
heartless-unicorn <pavlovamofficial@gmail.com>
2023-02-17 23:43:50 +0200

    added timer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import Timer from "./Timer";
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <div></div>
+      <Timer
+        minutes={0}
+        seconds={10}
+        step={100}
+        milsec={1000}
+        onTimeEnd={() => console.log("Час вийшов!")}
+        onTimeStart={(timeLeft) => console.log("Таймер запущено!")}
+        onTimePause={(timeLeft) => console.log("Таймер на паузі!")}
+        onTick={(time) => console.log("Залишилось часу: " + time)}
+      />
     </div>
   );
 }
--- src/Timer.js
+++ src/Timer.js
@@ -1,37 +1,89 @@
 import { Component } from "react";
 
 class Timer extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      time: 10,
+      interval: props.step,
+      minutes: props.minutes,
+      seconds: props.seconds * 1000,
+      milsec: props.milsec,
       active: true,
     };
-    this.tick = this.tick.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
   }
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.minutes === 0 && nextState.seconds === 0) {
+      this.props.onTimeEnd();
+      clearInterval(this.timerID);
+      return false;
+    }
+    return true;
   }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.time < 0) {
+  startTimer() {
+    console.log(this.state);
+    if (this.state.active) {
+      this.props.onTimeStart();
+      this.timerID = setInterval(() => this.tick(), this.state.interval);
+    } else {
+      this.props.onTimePause();
       clearInterval(this.timerID);
     }
-  }
-  //   componentWillUnmount() {
-  //
 
+    this.setState({ active: !this.state.active });
+  }
+  resetTimer() {
+    this.setState({
+      minutes: this.props.minutes,
+      seconds: this.props.seconds * 1000,
+      milsec: this.props.milsec,
+      active: true,
+    });
+  }
   tick() {
-    if (this.state.active) {
-      this.setState({ time: this.state.time-- });
+    // this.props.onTick(`${this.state.seconds / 1000} seconds`);
+    if (this.state.interval < 1000) {
+      if (this.state.milsec > 0) {
+        this.setState({ milsec: this.state.milsec - this.state.interval });
+      } else if (this.state.seconds === 0) {
+        this.setState({
+          seconds: 60000,
+          minutes: this.state.minutes - 1,
+        });
+      } else if (this.state.milsec === 0) {
+        this.setState({
+          seconds: this.state.seconds - 1000,
+          milsec: 1000 - this.state.interval,
+        });
+      }
+    } else {
+      if (this.state.seconds > 0) {
+        this.setState({ seconds: this.state.seconds - this.state.interval });
+      } else if (this.state.seconds === 0) {
+        this.setState({
+          minutes: this.state.minutes - 1,
+          seconds: 59000,
+        });
+      }
     }
   }
   render() {
     return (
       <div className="Timer">
-        <div>{this.state.time}</div>
-        <button onClick={() => this.setState({ active: !this.state.active })}>
-          Stop
-        </button>
+        <div>
+          {this.state.minutes < 10
+            ? `0${this.state.minutes}`
+            : this.state.minutes}
+          :
+          {this.state.seconds < 10
+            ? `0${this.state.seconds / 1000}`
+            : this.state.seconds / 1000}
+          {this.props.milsec ? `:${this.state.milsec}` : null}
+        </div>
+        <button onClick={this.startTimer}>Stop/Start</button>
+        <button onClick={this.resetTimer}>Reset</button>
       </div>
     );
   }
